9fee488099cb97e8592e6b6ef283e3540bd66b2d
User <>
2018-10-29 15:29:52 +0900

    edit user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :dogs, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :prefecture
+
+  validates :nickname, presence: true
 end
