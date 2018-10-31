426d75ac3ec3fba5697335ea59d531e3cc555471
User <>
2018-10-31 20:29:55 +0900

    add method to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
   end
@@ -34,4 +35,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :text, :category_id).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
