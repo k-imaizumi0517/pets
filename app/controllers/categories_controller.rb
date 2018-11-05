bb0f5fbbd9c6357308ce44cc6685f707a2de60b0
User <>
2018-11-06 02:04:28 +0900

    add show_action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  before_action :set_item, only: [:show]
+
+  def show
+    @posts = Post.where(category_id: @category.id).order("created_at DESC")
+  end
+
+  private
+
+  def set_item
+    @category = Category.find(params[:id])
+  end
 end
