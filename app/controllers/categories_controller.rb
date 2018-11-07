3699cef9e47e7965f516aae854e180efab976a30
User <>
2018-11-07 18:09:07 +0900

    edit categories_controller2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
 
   def show
-    @posts = Post.where(category_id: @category.id).order("created_at DESC").page(params[:page]).per(5)
+    @posts = Post.where(category_id: @category.id).order("created_at DESC").page(params[:page]).per(9)
   end
 
   private
