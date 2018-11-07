1b2673b404b946ce17d15d52c934ce106e4febe5
User <>
2018-11-07 16:57:22 +0900

    add show_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    @items = @user.posts.order("created_at DESC")
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
