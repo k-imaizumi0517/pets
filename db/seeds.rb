78fcd343e80603a5f093dd30921b22982d5717a6
User <>
2018-11-09 20:19:42 +0900

    add csv_file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach("db/type.csv") do |row|
+  Type.create(:name => row[0])
+end
