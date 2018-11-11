c905cd6f1512a3043a42e1c996a757ffbf8310d4
User <>
2018-11-11 19:02:05 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :move_to_top, except: [:show]
 
   def show
     @posts = @user.posts.order("created_at DESC").page(params[:page]).per(9)
@@ -7,13 +8,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    redirect_to root_path unless current_user.id == @user.id
   end
 
   def update
-    if current_user.update(user_params)
-      redirect_to user_path(current_user), notice: 'プロフィール登録しました'
-    else
-      render :edit
+    if current_user.id == @user.id
+      if current_user.update(user_params)
+        redirect_to user_path(current_user), notice: 'プロフィール登録しました'
+      else
+        render :edit
+      end
     end
   end
 
@@ -25,5 +29,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :image, :prefecture_id, :introduction)
+  end
+
+  def move_to_top
+    redirect_to root_path unless user_signed_in?
   end
 end
