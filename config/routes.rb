44d0438725aa46d8b455b97bb0ce97500ef93cce
User <>
2018-11-14 19:10:25 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
   resources :categories, only: [:show]
   resources :users, only: [:show, :edit, :update]
   resources :dogs, only: [:new, :create, :show, :edit, :update, :destroy]
